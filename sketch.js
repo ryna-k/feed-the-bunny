405941aacad10be5beebd50800a6302d838e2de0
ryna-k <102335559+ryna-k@users.noreply.github.com>
2023-01-31 19:37:28 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -10,9 +10,20 @@ const Composite = Matter.Composite;
 let engine;
 let world;
 var ground;
-var rope;
-var fruit;
+var fruit,rope;
 var fruit_con;
+
+var bg_img;
+var food;
+var bunny;
+var button;
+
+function preload()
+{
+  bg_img = loadImage('background.png');
+  food = loadImage('melon.png');
+  rabbit = loadImage('Rabbit-01.png');
+}
 
 function setup() 
 {
@@ -21,29 +32,46 @@ function setup()
   engine = Engine.create();
   world = engine.world;
   ground = new Ground(200,680,600,20);
-  rope = new Rope(6, {x:245, y:30});
+  bunny = createSprite(250,650,100,100);
+  bunny.addImage(rabbit);
+  bunny.scale = 0.2;
   
-  var fruit_options = {
-    density: 0.001
-  }
-  fruit = Bodies.circle(300,300,20,fruit_options);
+  button = createImg('cut_button.png');
+  button.position(220,30);
+  button.size(50,50);
+  button.mouseClicked(drop);
+
+
+  rope = new Rope(7,{x:245,y:30});
+  fruit = Bodies.circle(300,300,20);
   Matter.Composite.add(rope.body,fruit);
-  fruit_con = new Link(rope, fruit);
-  
+
+  fruit_con = new Link(rope,fruit);
+
   rectMode(CENTER);
   ellipseMode(RADIUS);
   textSize(50)
+  imageMode(CENTER);
   
 }
 
 function draw() 
 {
   background(51);
-  ground.show();
+
+  image(bg_img,width/2,height/2,490,690);
+
+  image(food,fruit.position.x,fruit.position.y,70,70);
   rope.show();
-  ellipse(fruit.position.x, fruit.position.y, 15, 15);
-
   Engine.update(engine);
+  ground.show();
 
-  
+ drawSprites();
+   
+}
+
+function drop(){
+  rope.break();
+  fruit_con.detatch();
+  fruit_con = null;
 }
