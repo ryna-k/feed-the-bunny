b586221abe077a4121aed5b9bee2879d2fc13fe9
ryna-k <102335559+ryna-k@users.noreply.github.com>
2023-02-21 19:33:54 -0500

    Add files via upload

--- link.js
+++ link.js
@@ -2,10 +2,8 @@ class Link{
     constructor(bodyA,bodyB)
     {
       var lastlink = bodyA.body.bodies.length-2;
-      console.log(lastlink);
-      this.link = Constraint.create(
+     this.link = Constraint.create(
         {
-          
           bodyA:bodyA.body.bodies[lastlink],
           pointA:{x:0,y:0},
           bodyB:bodyB,
